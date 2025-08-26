93763bf20cfdfe92d3f4a726a476d8553f82cf5c
Hamidreza <hamidreza@Ubuntu22.04LTS>
2025-08-26 15:31:47 +0200

    fix network issues

--- client-ping-matrix.sh
+++ client-ping-matrix.sh
@@ -3,8 +3,9 @@
 clients_and_edges=(
   client1 client2 client3 client4 client5 client6 client7 client8 client9 client10
   client11 client12 client13 client14 client15 client16 client17 client18 client19 client20
-  client21 client22 client23 client24 client25 client26 client27
-  Edge_server1 Edge_server2 Edge_server3 Edge_server4 Edge_server5 Edge_server6 Edge_server7 Edge_server8
+  client21 client22 client23 client24 client25 client26 client27 client28 client29 client30
+  client31 client32 client33 client34 client35
+  Edge_server1 Edge_server2 Edge_server3 Edge_server4 Edge_server5 Edge_server6 Edge_server7 Edge_server8 Edge_server9
 )
 
 echo -e "Ping Matrix (eth1):\n"
